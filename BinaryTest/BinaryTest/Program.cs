5365ca14ecec184e88786e4c1213b58e86703e13
Elvvinn <tu7hptr3b@code.edu.az>
2022-11-02 13:30:59 -0700

    Presentation and code

---
+++ BinaryTest/BinaryTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] ededler = { 9, 50, 23, 34, 19 };
+            int kicik, orta, boyuk, axtarilaneded = 19;
+            kicik = 0;
+            boyuk = ededler.Length - 1;
+
+            while (kicik <= boyuk)
+            {
+                orta = (kicik + boyuk) / 2;
+                if (axtarilaneded == ededler[orta])
+                {
+                    Console.WriteLine("ededimizin arrayin {0} . deyeri ", orta);
+                    break;
+                }
+
+                else if (axtarilaneded < ededler[orta])
+                {
+                    boyuk = orta - 1;
+                  
+                }
+                else
+                {
+                    kicik = orta + 1;
+                    break;
+                }
+                Console.WriteLine(axtarilaneded);
+              
+            }
+        }
+    }
+}
---
+++ LinearTest/LinearTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] ededler  = new int[5] { 8, 10, 20, 30, 40 };
+            int axtarilaneded = 10;
+            for (int i = 0; i < ededler.Length; i++)
+            {
+                if (ededler[i]==axtarilaneded)
+                {
+                    Console.WriteLine("Axtardigimiz eded :" +axtarilaneded);
+                    
+                }
+            }
+
+        }
+    }
+}
